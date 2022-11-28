fc0dc7d2a1ee32acd4b69939ceb0fbe71a2964a0
NoyonAhammadKhan <noyonahammadkhan6464@gmail.com>
2022-11-28 16:50:40 -0500

    navbar completed

--- src/pages/Shared/Navbar/Navbar.js
+++ src/pages/Shared/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../Context/AuthProvider/AuthProvider';
 import useAdmin from '../../../hooks/useAdmin';
 import useSeller from '../../../hooks/useSeller';
-
+import logo from '../../../assets/logo/logo.png'
 const Navbar = () => {
   const {user,logOut}=useContext(AuthContext);
   
@@ -20,8 +20,8 @@ const Navbar = () => {
            <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
   <div className="container flex flex-wrap items-center justify-between mx-auto">
   <a href="https://flowbite.com/" className="flex items-center">
-      <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-      <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+      <img src={logo} className="h-16 mr-3 sm:h-9" alt="Flowbite Logo" />
+      <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Bookish Hero</span>
   </a>
   <div className="flex md:order-2">
       <Link to='/login'>
@@ -39,12 +39,8 @@ const Navbar = () => {
   </div>
   <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
     <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-    <li>
-        <Link to='/blogs' className="block text-xl font-bold py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Blogs</Link>
-      </li>
-      <li>
-        <Link to='/blogs' className="block text-xl font-bold py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Blogs</Link>
-      </li>
+  
+      
       <li>
         <Link to='/blogs' className="block text-xl font-bold py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Blogs</Link>
       </li>
