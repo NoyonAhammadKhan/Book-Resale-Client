c9c2c5c28ce764c920226e5f49957161bd863386
NoyonAhammadKhan <noyonahammadkhan6464@gmail.com>
2022-11-28 17:15:32 -0500

    blog page done

--- src/pages/Blogs/Blogs/Blogs.js
+++ src/pages/Blogs/Blogs/Blogs.js
@@ -1,9 +1,30 @@
+import axios from 'axios';
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Blog from '../Blog/Blog';
 
 const Blogs = () => {
+    const [blogs,setBlogs]=useState([]);
+    useEffect(()=>{
+        axios.get('https://usedbook-noyonahammadkhan.vercel.app/blogs')
+        .then(res=>{
+            setBlogs(res.data);
+            console.log(res.data)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    },[])
     return (
         <div>
             <h1 className='text-6xl'>This is blog page</h1>
+            <div className='mx-auto'>
+            {
+            blogs.map(blog=><Blog blog={blog} key={blog._id}></Blog>)
+             }
+            </div>
+         
         </div>
     );
 };
